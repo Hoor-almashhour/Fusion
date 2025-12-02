50933dce66610d083831ebb93ecd2948f3b514cb
HOOR ALMASHHOUR <houralmashhour@gmail.com>
2025-12-03 02:40:00 +0300

    update all

--- src/app/[locale]/countries/[country]/page.tsx
+++ src/app/[locale]/countries/[country]/page.tsx
@@ -1,22 +1,24 @@
 
-import { useLocale, useTranslations } from "next-intl";
-import { use } from "react";
+import { useTranslations } from "next-intl";
 import Hero from "./components/Hero";
-import Intro from "./components/Intro";
+
 import Stats from "./components/Stats";
 import Universities from "./components/Universities";
 import WhyStudy from "./components/WhyStudy";
 
-export default function CountryPage({
-   params,
-    }: {
-  params: { locale: string; country: string };
-    }) {
-      const { country, locale } = params;
+interface CountryPageProps {
+  params: {
+    locale: string;
+    country: string;
+  };
+}
 
-    const t = useTranslations(country);
-   const isArabic = locale === "ar";
+export default function CountryPage({ params }: CountryPageProps) {
+  const { country, locale } = params;
 
+  const t = useTranslations(country);
+  const isArabic = locale === "ar";
+ 
 
   return (
     <main className={`flex flex-col items-center bg-white ${isArabic ? "rtl" : "ltr"}`}>
