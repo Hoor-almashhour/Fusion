75b6aaa9097bcf37f8d49b426ca1ca2b802e4609
HOOR ALMASHHOUR <houralmashhour@gmail.com>
2025-12-04 00:41:37 +0300

    update

--- src/app/[locale]/countries/[country]/page.tsx
+++ src/app/[locale]/countries/[country]/page.tsx
@@ -1,33 +1,39 @@
-import { getTranslations } from "next-intl/server";
+
+import { useTranslations } from "next-intl";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
 import Universities from "./components/Universities";
 import WhyStudy from "./components/WhyStudy";
+import { getTranslations } from "next-intl/server";
 
 export async function generateStaticParams() {
+  // يمكنك إرجاع كل الدول المتاحة هنا
   return [
-    { country: "turkey", locale: "ar" },
-    { country: "turkey", locale: "en" },
+    { country: "turkey" },
+   
+    // إضافة دول أخرى هنا
   ];
 }
 
 export default async function CountryPage({
   params,
 }: {
-  params: { locale: string; country: string };
+  params: Promise<{ locale: string; country: string }>;
 }) {
+  const { locale, country } = await params;
 
-  const { locale, country } = params;
-
-  const t = await getTranslations({ locale, namespace: country });
-
+    const t = await getTranslations({ locale, namespace: country });
   const isArabic = locale === "ar";
+ 
 
   return (
     <main className={`flex flex-col items-center bg-white ${isArabic ? "rtl" : "ltr"}`}>
       <Hero t={t} />
       <Stats t={t} />
+     
+    
       <WhyStudy t={t} />
+     
       <Universities t={t} />
     </main>
   );
